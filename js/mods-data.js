fdd1ee9051bb0a055906a409e093cebfecab6570
Malloy <lefehanims@gmail.com>
2025-11-20 19:19:07 -0600

    kjksakjkjassa trapos de silly kids

--- js/mods-data.js
+++ js/mods-data.js
@@ -1,4 +1,6 @@
 const MOD_FOLDERS = [
   { slug: "vs-guy-named-laumber-mobile", categories: ["featured"] },
-  { slug: "vs-memstar", categories: ["upcoming"] }
+  { slug: "vs-memstar", categories: ["upcoming"] },
+  { slug: "trap-4-silly-kids", categories: ["featured"] },
+  //{ slug: "vs-torron de mierda", categories: ["featured"] }
 ];
